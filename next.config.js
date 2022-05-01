5a8003b1d37679582441254db974a625bb632d67
Yash Srivastava <yashaqua@gmail.com>
2022-05-01 23:23:51 +0530

    add svg support

--- next.config.js
+++ next.config.js
@@ -3,5 +3,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['rb.gy'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 }
