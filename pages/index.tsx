4a50d2252e1193b8a1889475e2e5fe251c72d0eb
Yash Srivastava <yashaqua@gmail.com>
2022-05-04 23:08:07 +0530

    Add row, thumbnail component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../Components/Banner'
 import Header from '../Components/Header'
+import Row from '../Components/Row'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 
@@ -27,27 +28,36 @@ const Home = ({
   trendingNow,
 }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div
+      className={`} relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] 
+      lg:h-[140vh]`}
+    >
       <Head>
-        <title>Home-Netflix</title>
+        <title>
+          {/* {movie?.title || movie?.original_name || 'Home'} - Netflix */}
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-      <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
-        {/* Banner */}
+
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {/* Modal */}
+      {/* {showModal && <Modal />} */}
     </div>
   )
 }
