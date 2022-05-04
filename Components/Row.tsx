90a44b2286655948c0ef1509111a05efd48ee58e
Yash Srivastava <yashaqua@gmail.com>
2022-05-05 00:17:50 +0530

    add carousel effect for movie titles

--- Components/Row.tsx
+++ Components/Row.tsx
@@ -6,11 +6,26 @@ import Thumbnail from './Thumbnail'
 
 interface Props {
   title: string
+  // movies: Movie[] | DocumentData[]
   movies: Movie[]
 }
 
 function Row({ title, movies }: Props) {
+  const rowRef = useRef<HTMLDivElement>(null)
   const [isMoved, setIsMoved] = useState(false)
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true)
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current
+
+      const scrollTo =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth
+      rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+    }
+  }
 
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
@@ -22,13 +37,20 @@ function Row({ title, movies }: Props) {
           className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
             !isMoved && 'hidden'
           }`}
+          onClick={() => handleClick('left')}
         />
-        <div className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2">
+        <div
+          ref={rowRef}
+          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
+        >
           {movies.map((movie) => (
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-        <ChevronRightIcon className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+        <ChevronRightIcon
+          className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          onClick={() => handleClick('right')}
+        />
       </div>
     </div>
   )
