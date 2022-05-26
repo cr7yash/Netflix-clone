d52f83abbce5e1894a91185b6756bd86df31ad4b
Yash Srivastava <yashaqua@gmail.com>
2022-05-26 23:45:12 +0530

    complete useAuth hook

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,4 +1,3 @@
-import { async } from '@firebase/util'
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
@@ -11,33 +10,76 @@ import { useRouter } from 'next/router'
 import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import { auth } from '../firebase'
 
-const useAuth = () => {
-  const [loading, setLoading] = useState(false)
-  const [user, setUser] = useState<User | null>(null)
+interface IAuth {
+  user: User | null
+  signUp: (email: string, password: string) => Promise<void>
+  signIn: (email: string, password: string) => Promise<void>
+  logout: () => Promise<void>
+  error: string | null
+  loading: boolean
+}
 
+const AuthContext = createContext<IAuth>({
+  user: null,
+  signUp: async () => {},
+  signIn: async () => {},
+  logout: async () => {},
+  error: null,
+  loading: false,
+})
+
+interface AuthProviderProps {
+  children: React.ReactNode
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
   const router = useRouter()
+  const [user, setUser] = useState<User | null>(null)
+  const [error, setError] = useState(null)
+  const [initialLoading, setInitialLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // Logged in...
+          setUser(user)
+          setLoading(false)
+        } else {
+          // Not logged in...
+          setUser(null)
+          setLoading(true)
+          router.push('/login')
+        }
+
+        setInitialLoading(false)
+      }),
+    [auth]
+  )
 
   const signUp = async (email: string, password: string) => {
     setLoading(true)
 
     await createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        setUser(userCredential.user), router.push('/')
+        setUser(userCredential.user)
+        router.push('/')
         setLoading(false)
       })
-      .catch((err) => alert(err.message))
+      .catch((error) => alert(error.message))
       .finally(() => setLoading(false))
   }
 
   const signIn = async (email: string, password: string) => {
     setLoading(true)
-
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        setUser(userCredential.user), router.push('/')
+        setUser(userCredential.user)
+        router.push('/')
         setLoading(false)
       })
-      .catch((err) => alert(err.message))
+      .catch((error) => alert(error.message))
       .finally(() => setLoading(false))
   }
 
@@ -48,11 +90,24 @@ const useAuth = () => {
       .then(() => {
         setUser(null)
       })
-      .catch((err) => alert(err.message))
+      .catch((error) => alert(error.message))
       .finally(() => setLoading(false))
   }
 
-  return <div>useAuth</div>
+  const memoedValue = useMemo(
+    () => ({ user, signUp, signIn, error, loading, logout }),
+    [user, loading, error]
+  )
+
+  return (
+    <AuthContext.Provider value={memoedValue}>
+      {!initialLoading && children}
+    </AuthContext.Provider>
+  )
 }
 
-export default useAuth
+// Let's only export the `useAuth` hook instead of the context.
+// We only want to use the hook directly and never the context comopnent.
+export default function useAuth() {
+  return useContext(AuthContext)
+}
